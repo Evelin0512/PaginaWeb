429e60f09680992f856f50966a4953208e4fd67e
Evelin <evelinshz432@gmail.com>
2026-06-08 18:52:36 -0600

    Primer commit

---
+++ Servidor.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+
+    fs.readFile('index.html', (err, data) => {
+
+        if (err) {
+            res.writeHead(404);
+            res.end('Archivo no encontrado');
+            return;
+        }
+
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+
+        res.end(data);
+
+    });
+
+}).listen(3000);
+
+console.log('Servidor corriendo en http://localhost:3000');
